b6f5c327215009ccb8535b98cc9baa86906a002b
Nreis280 <nicolasreissanto@gmail.com>
2023-07-11 12:44:14 -0300

    feat: create News Schema

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,28 @@
-const login = async (req, res) => {
+import bcrypt, { compareSync } from "bcrypt";
+import { loginService, generateToken } from "../services/auth.service.js";
 
-  res.send("login ok");
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await loginService(email);
+
+    if(!user){
+      return res.status(404).send({message: "Invalid! User or Password not found"})
+    }
+
+    const passwordValid = bcrypt.compareSync(password, user.password);
+
+    if(!passwordValid){
+      return res.status(404).send({message: "Invalid! User or Password not found"})}
+    
+
+    const token =  generateToken(user.id)
+
+    res.send({token});
+    console.log(passwordValid)
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 };
 
-export default {login}
+export default { login };
---
+++ src/services/auth.service.js
@@ -0,0 +1,9 @@
+import User from "../models/User.js";
+import Jwt from "jsonwebtoken";
+
+const loginService = (email) =>
+  User.findOne({ email: email }).select("+password");
+
+  const generateToken = (id) => Jwt.sign({id: id}, process.env.SECRET_JWT, {expiresIn: 86400});
+
+export { loginService, generateToken };
