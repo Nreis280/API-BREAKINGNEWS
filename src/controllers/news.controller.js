0f2d255323284a6b848b73b58325656e1b243642
Nreis280 <nicolasreissanto@gmail.com>
2023-07-19 12:04:20 -0300

    Search by title and news by user

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -22,32 +22,34 @@ const create = async (req, res) => {
 };
 
 const findAll = async (req, res) => {
-  try{
+  try {
     let { limit, offset } = req.query;
 
     limit = Number(limit);
     offset = Number(offset);
-  
+
     if (!limit) {
       limit = 5;
     }
-  
+
     if (!offset) {
       offset = 0;
     }
-  
+
     const news = await newsService.findAllService(offset, limit);
     const total = await newsService.countNews();
     const currentUrl = req.baseUrl;
-  
+
     const next = offset + limit;
     const nextUrl =
       next < total ? `${currentUrl}?limit=${limit}&offset=${offset}` : null;
-  
+
     const previous = offset - limit < 0 ? null : offset - limit;
     const previousUrl =
-      previous != null ? `${currentUrl}?limit=${limit}&offset=${previous}` : null;
-  
+      previous != null
+        ? `${currentUrl}?limit=${limit}&offset=${previous}`
+        : null;
+
     if (news.length === 0) {
       return res.status(400).send({ menssage: "there are no registered news" });
     }
@@ -57,7 +59,7 @@ const findAll = async (req, res) => {
       limit,
       offset,
       total,
-  
+
       results: news.map((Item) => ({
         id: Item._id,
         title: Item.title,
@@ -73,46 +75,18 @@ const findAll = async (req, res) => {
   } catch (err) {
     res.status(500).send({ message: err.message });
   }
-
 };
 
 const topNews = async (req, res) => {
-  try{
+  try {
     const news = await newsService.topNewsService();
 
     if (!news) {
-      return res.status(400).send({menssage: "There is no registered post"})
+      return res.status(400).send({ menssage: "There is no registered post" });
     }
-  
-  
-    res.send({
-        news:{
-          id: news._id,
-          title: news.title,
-          text: news.text,
-          banner: news.banner,
-          likes: news.likes,
-          comments: news.comments,
-          name: news.user.name,
-          userName: news.user.username,
-          userAvatar: news.user.avatar,
-        }
-    })
-  }
-  catch (err) {
-    res.status(500).send({ message: err.message });
-  }
-}
-
-const findeById = async (req, res) => {
-  try{
-
-    const {id}= req.params;
-
-    const news = await newsService.findByIdService(id)
 
     res.send({
-      news:{
+      news: {
         id: news._id,
         title: news.title,
         text: news.text,
@@ -122,10 +96,88 @@ const findeById = async (req, res) => {
         name: news.user.name,
         userName: news.user.username,
         userAvatar: news.user.avatar,
-      }
-    })
-  }catch (err) {
+      },
+    });
+  } catch (err) {
     res.status(500).send({ message: err.message });
   }
-}
-export default { create, findAll, topNews, findeById, };
+};
+
+const findeById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const news = await newsService.findByIdService(id);
+
+    return res.send({
+      news: {
+        id: news._id,
+        title: news.title,
+        text: news.text,
+        banner: news.banner,
+        likes: news.likes,
+        comments: news.comments,
+        name: news.user.name,
+        userName: news.user.username,
+        userAvatar: news.user.avatar,
+      },
+    });
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+};
+
+const searchByTitle = async (req, res) => {
+  try {
+    const { title } = req.query;
+
+    const news = await newsService.searchByTitleService(title);
+
+    if (news.length === 0) {
+      return res
+        .status(400)
+        .send({ menssage: "There are no news whit this title" });
+    }
+
+    return res.send({
+      results: news.map((Item) => ({
+        id: Item._id,
+        title: Item.title,
+        text: Item.text,
+        banner: Item.banner,
+        likes: Item.likes,
+        comments: Item.comments,
+        name: Item.user.name,
+        userName: Item.user.username,
+        userAvatar: Item.user.avatar,
+      })),
+    });
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+};
+
+const byUser = async (req, res) => {
+  try {
+    const id = req.userId;
+    const news = await newsService.byUserService(id);
+
+    return res.send({
+      results: news.map((Item) => ({
+        id: Item._id,
+        title: Item.title,
+        text: Item.text,
+        banner: Item.banner,
+        likes: Item.likes,
+        comments: Item.comments,
+        name: Item.user.name,
+        userName: Item.user.username,
+        userAvatar: Item.user.avatar,
+      })),
+    });
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+};
+
+export default { create, findAll, topNews, findeById, searchByTitle, byUser};
--- src/routes/news.route.js
+++ src/routes/news.route.js
@@ -7,5 +7,9 @@ import  {authMiddleware}  from "../middlewares/auth.middleware.js";
 router.post("/", authMiddleware ,newsController.create);
 router.get("/", newsController.findAll);
 router.get("/top", newsController.topNews);
-router.get("/:id", newsController.findeById);
+router.get("/search", newsController.searchByTitle);
+
+router.get("/byUser", authMiddleware, newsController.byUser);
+
+router.get("/:id",newsController.findeById);
 export default router;
